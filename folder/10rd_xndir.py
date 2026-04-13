b02ca84f21fb06797273e8eec8d18bb07ff72ea9
AlbertPogh <albert30062009@gmail.com>
2026-04-13 00:14:05 -0700

    Implement function to filter even numbers from list
    Added a new function 'zuyg' to filter even numbers from a list.

--- folder/10rd_xndir.py
+++ folder/10rd_xndir.py
@@ -1,3 +1,5 @@
+#2rd tarberaki 10y
+
 def duplicate(string):
     datark=""
     for x in string:
@@ -5,3 +7,12 @@ def duplicate(string):
             datark+=x
     return datark
 print(duplicate("lavutyun"))
+
+#1in tarberaki 10y
+def zuyg(listt):
+    b=[]
+    for x in listt:
+        if x%2==0:
+            b.append(x)
+    return b
+print(zuyg([1,2,3,4,5,6,7,8,9,10]))
